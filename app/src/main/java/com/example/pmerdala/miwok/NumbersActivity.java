18ee5f98f4a6c65e63cb3fae0da4565041e95ce1
Paweł Merdała <p.merdala@interia.pl>
2017-10-27 15:57:51 +0200

    add numers english words

--- app/src/main/java/com/example/pmerdala/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/pmerdala/miwok/NumbersActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class NumbersActivity extends AppCompatActivity {
@@ -13,6 +14,29 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        String[] words = initEnglishNumbers();
+        displayWords(words);
+    }
+
+    private String[] initEnglishNumbers(){
+        String[] englishNumbers = new String[10];
+        englishNumbers[0]="one";
+        englishNumbers[1]="two";
+        englishNumbers[2]="free";
+        englishNumbers[3]="four";
+        englishNumbers[4]="five";
+        englishNumbers[5]="six";
+        englishNumbers[6]="seven";
+        englishNumbers[7]="eight";
+        englishNumbers[8]="nine";
+        englishNumbers[9]="ten";
+        return englishNumbers;
+    }
+
+    private void displayWords(String[] words){
+        for (int i=0;i<10;i++) {
+            Log.v("NumbersActivity", "Word at index 0: " + words[i]);
+        }
     }
 
 }
