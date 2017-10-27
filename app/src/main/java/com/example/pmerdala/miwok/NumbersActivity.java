3b03e529f1442ac2af06569eef251b218b1c671b
Paweł Merdała <p.merdala@interia.pl>
2017-10-27 16:45:55 +0200

    change from Array to ArrayList

--- app/src/main/java/com/example/pmerdala/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/pmerdala/miwok/NumbersActivity.java
@@ -7,6 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -14,29 +19,43 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        String[] words = initEnglishNumbers();
+        List<String> words = initEnglishNumbers();
         displayWords(words);
     }
 
-    private String[] initEnglishNumbers(){
-        String[] englishNumbers = new String[10];
-        englishNumbers[0]="one";
-        englishNumbers[1]="two";
-        englishNumbers[2]="free";
-        englishNumbers[3]="four";
-        englishNumbers[4]="five";
-        englishNumbers[5]="six";
-        englishNumbers[6]="seven";
-        englishNumbers[7]="eight";
-        englishNumbers[8]="nine";
-        englishNumbers[9]="ten";
+    private List<String> initEnglishNumbers(){
+        List<String> englishNumbers = new ArrayList<String>();
+        englishNumbers.add("one");
+        englishNumbers.add("two");
+        englishNumbers.add("three");
+        englishNumbers.add("four");
+        englishNumbers.add("five");
+        englishNumbers.add("six");
+        englishNumbers.add("seven");
+        englishNumbers.add("eight");
+        englishNumbers.add("nine");
+        englishNumbers.add("ten");
         return englishNumbers;
     }
 
-    private void displayWords(String[] words){
-        for (int i=0;i<10;i++) {
-            Log.v("NumbersActivity", "Word at index 0: " + words[i]);
+    private void displayWords(List<String> words){
+        LinearLayout parentView = (LinearLayout) findViewById(R.id.numbersView);
+        int i = 0;
+        while(i < 10){
+//        for (int i=0;i<10;i++) {
+//            Log.v("NumbersActivity", "Word at index " + i+": " + words.get(i));
+            parentView.addView(createTextView(words.get(i)));
+            i++;
         }
+    }
+
+    private TextView createTextView(String word ){
+        TextView textView = new TextView(getBaseContext());
+        textView.setText(word);
+        textView.setTextSize(24);
+        textView.setPadding(16,16,16,16);
+        textView.setBackgroundColor(getResources().getColor(R.color.category_numbers));
+        return textView;
     }
 
 }
